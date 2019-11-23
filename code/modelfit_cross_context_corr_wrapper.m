45c4dd8f8eab1556d7c1852c72d47a613990523b
Sam Norman-Haignere <snormanhaignere@gmail.com>
2019-11-22 19:21:49 -0700

    modelfit_cross_context_corr_wrapper.m, major changes, more flexible

--- code/modelfit_cross_context_corr_wrapper.m
+++ code/modelfit_cross_context_corr_wrapper.m
@@ -1,24 +1,43 @@
-function M_all = modelfit_cross_context_corr_wrapper(...
-    L, groupnames, figure_directory, output_directory, varargin)
+function M = modelfit_cross_context_corr_wrapper(...
+    L, modelfit_params, output_directory, figure_directory, varargin)
 
 % The dominant computational cost for model fitting is the number of model windows tested not
 % the number of channels. So this function combines data across multiple
 % lagged correlation analyses to reduce computational cost.
+% 
+% 2019-11-22: Reshape back to input, plus other mods, Sam NH
 
-% number of lagged correlation analyses
+dims = size(L);
+L = L(:);
+
+% number of lagged correlation structures to combine across
 n_groups = length(L);
-assert(n_groups == length(groupnames));
+
+% default name for each structure
+I.groupnames = cell(1, n_groups);
+for i = 1:n_groups
+    I.groupnames{i} = ['group' num2str(i)];
+end
+
+% overwrite with optional inputs
+I = parse_optInputs_keyvalue(varargin, I);
 
 % combine
 L_all = L{1};
-L_all.chnames = strcat(groupnames{1}, L{1}.chnames);
+L_all.chnames = strcat(I.groupnames{1}, '-', L{1}.chnames);
 for i = 2:n_groups
+    
+    % combine correlation, combine over channel dimension
     L_all.same_context = cat(3, L_all.same_context, L{i}.same_context);
     L_all.diff_context = cat(3, L_all.diff_context, L{i}.diff_context);
+    
+    % misc adaptation parameters, combine over channel dimension
     L_all.mean_diff_tc = cat(3, L_all.mean_diff_tc, L{i}.mean_diff_tc);
     L_all.mean_diff_tc_segdur = cat(4, L_all.mean_diff_tc_segdur, L{i}.mean_diff_tc_segdur);
-    L_all.chnames = [L_all.chnames, strcat(groupnames{i}, L{i}.chnames)];
-    L_all.channels = cat(2, L_all.channels, (1:length(L{i}.channels)) + length(L_all.channels));
+    
+    % combine across channels and group names, add group name to the channel name
+    L_all.chnames = [L_all.chnames, strcat(I.groupnames{i}, '-', L{i}.chnames)];
+    L_all.channels = cat(2, L_all.channels, L{i}.channels);
     
     % check to make sure they have the same parameters
     f_to_check = {'lag_t', 'sr', 'unique_segs', 'rampwin', 'rampdur'};
@@ -30,7 +49,21 @@ end
 % run
 L_all.figure_directory = figure_directory;
 L_all.output_directory = output_directory;
-M_all = modelfit_cross_context_corr(L_all, varargin{:});
+M_all = modelfit_cross_context_corr(L_all, modelfit_params{:});
 
-    
-
+% split back up
+M = cell(dims);
+for i = 1:length(L)
+    index = 0;
+    n_channels = length(L{i}.channels);
+    chan = (1:n_channels) + index;
+    M{i} = M_all;
+    M{i}.Y = M_all.Y(:,:,chan);
+    M{i}.loss = M_all.loss(:,:,:,chan);
+    M{i}.Ybest = M_all.Ybest(:,:,chan);
+    M{i}.best_intper_sec = M_all.best_intper_sec(chan,:);
+    M{i}.best_delay_sec = M_all.best_delay_sec(chan,:);
+    M{i}.best_shape = M_all.best_shape(chan,:);
+    M{i}.best_loss = M_all.best_loss(chan,:);
+    M{i}.channels = M_all.channels(chan);
+end
